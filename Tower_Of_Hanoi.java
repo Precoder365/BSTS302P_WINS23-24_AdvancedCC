dfc75d90d0cf8cd0aeb1b4e23784cf5e90922178
Prenitha R <91544325+Precoder365@users.noreply.github.com>
2024-03-13 01:16:16 +0530

    Update Tower_Of_Hanoi.java

--- Tower_Of_Hanoi.java
+++ Tower_Of_Hanoi.java
@@ -1,20 +1,19 @@
 import java.util.*;
 
-class Main { 
-    static void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod) 
-    { 
-        if (n == 0) { 
-            return; 
-        } 
-        towerOfHanoi(n - 1, from_rod, aux_rod, to_rod); 
-        System.out.println("Move disk " + n + " from rod "+ from_rod + " to rod "+ to_rod); 
-        towerOfHanoi(n - 1, aux_rod, to_rod, from_rod); 
-    } 
-  
-    public static void main(String args[]) 
-    { 
+class Main{
+    public static void toh(int n, char from, char to, char aux){
+        if(n==0) return;
+        
+        toh(n-1,from,aux,to);
+        System.out.println("Disk "+n+" from rod "+from+" to rod "+to);
+        toh(n-1,aux,to,from);
+    }
+    
+    public static void main(String[] args){
+        int n;
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        towerOfHanoi(n, 'A', 'C', 'B'); 
-    } 
-} 
+        n=sc.nextInt();
+        
+        toh(n,'A','C','B');
+    }
+}
