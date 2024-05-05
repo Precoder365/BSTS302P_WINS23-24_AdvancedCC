2c3e5e72f057dac274c41f8dc50b326dd2a4e1e6
Prenitha R <91544325+Precoder365@users.noreply.github.com>
2024-05-05 11:53:49 +0530

    Update - add user input

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -1,39 +1,48 @@
-public class Main {
-    public static String longestCommonSubsequence(String text1, String text2) {
-        int m = text1.length();
-        int n = text2.length();
-        
-        int[][] dp = new int[m + 1][n + 1];
-        
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-            }
-        }
-        
-        StringBuilder lcs = new StringBuilder();
-        int i = m, j = n;
-        while (i > 0 && j > 0) {
-            if (text1.charAt(i-1) == text2.charAt(j-1)) {
-                lcs.insert(0, text1.charAt(i - 1));
-                i--;
-                j--;
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                i--;
-            } else {
-                j--;
-            }
-        }
-        
-        return lcs.toString();
-    }
+import java.util.*;
 
-    public static void main(String[] args) {
-        String text1 = "abcdef";
-        String text2 = "ace";
-        System.out.println("Longest Common Subsequence: " + longestCommonSubsequence(text1, text2));
+public class Main{
+    public static String lcs(String s1, String s2){
+        int m=s1.length();
+        int n=s2.length();
+        
+        int[][] L=new int[m+1][n+1];
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    L[i][j]=L[i-1][j-1]+1;
+                }
+                else{
+                    L[i][j]=Math.max(L[i-1][j],L[j][i-1]);
+                }
+            }
+        }
+        
+        StringBuilder sb=new StringBuilder();
+        int i=m;
+        int j=n;
+        while(i>0 && j>0){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                sb.insert(0, s1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if (L[i-1][j]>L[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        String s2=sc.next();
+        
+        System.out.println(lcs(s1,s2));
     }
 }
