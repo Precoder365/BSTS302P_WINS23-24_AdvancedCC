70c734b801e2183b1a76df95d6cffc6af412e6ab
Prenitha R <91544325+Precoder365@users.noreply.github.com>
2024-05-05 14:34:56 +0530

    Recursive solution

--- SubsetSum.java
+++ SubsetSum.java
@@ -1,37 +1,29 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Main {
-    static boolean isSubsetSum(int set[], int n, int sum) {
-        boolean dp[][] = new boolean[n + 1][sum + 1];
-
-        // If sum is 0, then answer is true
-        for (int i = 0; i <= n; i++)
-            dp[i][0] = true;
-
-        // If sum is not 0 and set is empty, then answer is false
-        for (int i = 1; i <= sum; i++)
-            dp[0][i] = false;
-
-        // Fill the dp table in bottom-up manner
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= sum; j++) {
-                if (j < set[i - 1])
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - set[i - 1]];
-            }
+public class Main{
+    public static boolean checkIfSubsetExists(int[] arr,int n,int k){
+        if (k==0) return true;
+        if (n==0) return false;
+        if (arr[n-1]>k){
+            return checkIfSubsetExists(arr,n-1,k);
         }
-
-        return dp[n][sum];
+        else{
+            return checkIfSubsetExists(arr,n-1,k) || checkIfSubsetExists(arr,n-1,k-arr[n-1]);
+        }
     }
-
-    public static void main(String args[]) {
-        int set[] = { 3, 34, 4, 12, 5, 2 };
-        int sum = 9;
-        int n = set.length;
-        if (isSubsetSum(set, n, sum))
-            System.out.println("Found a subset with given sum");
-        else
-            System.out.println("No subset with given sum");
+    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        
+        int k=sc.nextInt();
+        
+        if(checkIfSubsetExists(arr,n,k)) System.out.println("Exists");
+        else System.out.println("DNE");
     }
 }
