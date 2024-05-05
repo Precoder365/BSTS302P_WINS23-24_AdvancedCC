8029c0f02f17c1f43dde454fc65b70151344e728
Prenitha R <91544325+Precoder365@users.noreply.github.com>
2024-05-05 14:14:27 +0530

    Print the LPS

--- LongestPalindromicSubsequence.java
+++ LongestPalindromicSubsequence.java
@@ -1,18 +1,50 @@
-public class Main {
-    public static int findLongestPalindrome(String X, int i, int j) {
-        if(i>j) return 0;
-        if (i==j) return 1;
+import java.util.*;
 
-        if (X.charAt(i) == X.charAt(j)) {
-            return findLongestPalindrome(X, i + 1, j - 1) + 2;
+public class Main{
+    public static String lcs(String s1, String s2){
+        int m=s1.length();
+        int n=s2.length();
+        
+        int[][] L=new int[m+1][n+1];
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1)){
+                    L[i][j]=L[i-1][j-1]+1;
+                }
+                else{
+                    L[i][j]=Math.max(L[i-1][j],L[i][j-1]);
+                }
+            }
         }
-
-        return Integer.max(findLongestPalindrome(X, i, j - 1),findLongestPalindrome(X, i + 1, j));
+        
+        StringBuilder sb=new StringBuilder();
+        int i=m;
+        int j=n;
+        while(i>0 && j>0){
+            if(s1.charAt(i-1)==s2.charAt(j-1)){
+                sb.insert(0, s1.charAt(i-1));
+                i--;
+                j--;
+            }
+            else if (L[i-1][j]>L[i][j-1]){
+                i--;
+            }
+            else{
+                j--;
+            }
+        }
+        
+        return sb.toString();
     }
-
-    public static void main(String[] args) {
-        String X = "ABCDEF";
-        int n = X.length();
-        System.out.print(findLongestPalindrome(X, 0, n - 1));
+    
+    public static void main(String[] args){
+        // For longest palindromic do lcs with reverse of string
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.next();
+        StringBuilder reversed = new StringBuilder(s1).reverse();
+        String s2=reversed.toString();
+        
+        System.out.println(lcs(s1,s2));
     }
 }
