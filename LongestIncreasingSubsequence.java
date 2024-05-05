c82a2e2cb18c3300331acd8b9a3cc7ce61f68aad
Prenitha R <91544325+Precoder365@users.noreply.github.com>
2024-05-05 12:47:38 +0530

    Refactor code

--- LongestIncreasingSubsequence.java
+++ LongestIncreasingSubsequence.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 public class Main{
     public static int lis(int[] arr, int n){
+        if(arr==null || n==0) return -0;
+        
         int[] L=new int[n];
-    
-        L[0]=1;
-        for(int i=1;i<n;i++){
+        
+        // Initialize array with all 1s
+        for(int i=0;i<n;i++){
             L[i]=1;
+        }
+    
+        
+        for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 if(arr[i]>arr[j] && L[i]<L[j]+1){
                     L[i]=L[j]+1;
